57ccef2754308ee8c9cfee9783aa247f3a865dfb
matosr96 <19matos96@gmail.com>
2023-02-24 18:55:19 -0500

    fix: problem styles card-cover solved

--- apps/dashboard/src/screens/Cover/component/CardCover/index.tsx
+++ apps/dashboard/src/screens/Cover/component/CardCover/index.tsx
@@ -2,20 +2,12 @@ import React, { useEffect, useState } from "react";
 import styles from "./cardCover.module.css";
 import fiesta from "../../../../assets/fiesta.webp";
 import { useDispatch } from "react-redux";
-import {
-  deleteCover,
-  PartialCover,
-  updateCover,
-} from "@/redux/states/covers/covers";
+import { deleteCover, PartialCover, updateCover } from "@/redux/states/covers/covers";
 import EditCoverModal from "../../update";
 import { Cover } from "@partiaf/types";
 import { DivisaFormater } from "@/utils/DivisaFormater";
 import { Link } from "react-router-dom";
 import { PrivateRoutes } from "@/constants-definitions/Routes";
-
-import Swal from "sweetalert2";
-
-import { LoadingBox } from "./Loading";
 
 const CardCover = (Cover: any) => {
   const dispatch = useDispatch();
@@ -28,33 +20,14 @@ const CardCover = (Cover: any) => {
   const submitDeleteHandler = async (e: any) => {
     e.preventDefault();
     try {
-      Swal.fire({
-        title: "Quiere eliminar este evento?",
-        text: "No se podra revertir esta accion",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Si, eliminar",
-        cancelButtonText: "Cancelar",
-        width: "40em",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          dispatch(deleteCover(uuid) as any);
-          Swal.fire({
-            position: "center",
-            icon: "success",
-            title: "Evento eliminado",
-            showConfirmButton: false,
-            timer: 1800,
-          });
-        }
-      });
+      dispatch(deleteCover(uuid) as any);
     } catch (error) {
       if (error instanceof Error) {
+        console.log(error);
       }
     }
   };
+
 
   const [isOpenEdit, setIsOpenEdit] = useState(false);
   const [coverSelected, setCoverSelected] = useState<Cover>(Cover.cover);
@@ -68,7 +41,9 @@ const CardCover = (Cover: any) => {
     setStatus(!status);
     e.preventDefault();
     try {
-      dispatch(updateCover({ ...cover, status: status }) as any);
+      dispatch(
+        updateCover({...cover, status: status,}) as any
+      );
     } catch (error) {
       if (error instanceof Error) {
         console.log(error);
@@ -76,16 +51,13 @@ const CardCover = (Cover: any) => {
     }
   };
 
+
   return (
     <>
       <div className={styles.container_cover}>
       <div className={type == "VIP"?  styles.middle_line: styles.no_middle_line}></div>
         <Link className={styles.clickable} to={`${PrivateRoutes.COVERS}/${uuid}`}>
         </Link>
-        <Link
-          className={styles.clickable}
-          to={`${PrivateRoutes.COVERS}/${uuid}`}
-        ></Link>
         <div className={styles.image_cover}>
           {image ? (
             <img src={image} alt="Image" />
@@ -96,6 +68,7 @@ const CardCover = (Cover: any) => {
         <div className={styles.info_cover_container}>
           <span className={styles.sumer}>
             #SUMER
+            <span>2023</span>
           </span>
 
         <div className={styles.info_cover}>
@@ -134,50 +107,9 @@ const CardCover = (Cover: any) => {
               >
                 <p className="">Borrar</p>
               </button>
-          <div className={styles.info_cover}>
-            <div className={styles.data_cover}>
-              <p>{description}</p>
             </div>
-
-              <span>
-                <strong> Cupos:</strong> {limit}
-              </span>
-              <span>
-                <strong> Fecha:</strong> {date}
-              </span>
-              <span>
-                <strong> Hora:</strong> {hour}
-              </span>
-            </div>
-            <div className={styles.data_cover_price}>
-              <h5>VIP</h5>
-              <h4> {DivisaFormater(price)}</h4>
-            </div>
-          </div>
-          <div className={styles.icon_cover}>
-            <button
-              className={
-                cover.status
-                  ? styles.card_btn_status_active
-                  : styles.card_btn_status_inactive
-              }
-              onClick={submitUpdateHandler}
-            >
-              {cover.status ? "activo" : "inactivo"}
-            </button>
-            <button className={styles.btn_icon_card_cover}>
-              <p className="" onClick={() => editHandler()}>
-                Editar
-              </p>
-            </button>
-            <button
-              className={styles.btn_icon_card_cover_delete}
-              onClick={submitDeleteHandler}
-            >
-              <p className="">Borrar</p>
-            </button>
-          </div>
         </div>
+
       </div>
       <EditCoverModal
         openModal={isOpenEdit}
