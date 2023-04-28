1e26870f41d35515169db47cbf3ba2713e277c46
NestorMosqueraBmatch <nestor@bmatch.cl>
2023-04-28 11:03:55 -0500

    feat: add variables

--- apps/dashboard/src/api/index.ts
+++ apps/dashboard/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const PARTIAF_API = import.meta.env.MODE == 'development'?  axios.create({
-  baseURL: "http://localhost:8000/api/v3",
+  baseURL: "https://partiaf-api.xyz/api/v3",
 }) : axios.create({
   baseURL: "https://partiaf-api.xyz/api/v3",
 });
